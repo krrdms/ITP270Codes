8fb5b79ff5f754a50c75cd33c7638e7f610dc877
JKA <keirre.adams@gmail.com>
2021-07-18 02:43:00 -0400

    Sample code to show how assertions and try/catch works

---
+++ Modules/Module 5 Examples/AssertionExample.py
@@ -0,0 +1,12 @@
+try:
+    num_a = int(input("Enter a number between 1 and 5: "))
+    num_b = int(input("Enter a number between 5 and 10: "))
+    assert num_a >= 1, "number must be between greater or eq to 1"
+    assert num_a <= 5, "number must be less than or eq to 5"
+    assert num_b >= 5, "number must be between less than or eq to 5"
+    assert num_b <= 10, "number must be between greater or eq to 10"
+    num_c = num_b ** num_a
+    print("%d is the result" % num_c)
+
+except Exception as e:
+    print(e)
