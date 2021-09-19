447804ca83811a1bd242eb0dd0d1e0d620932692
JKA <keirre.adams@gmail.com>
2021-09-19 07:52:33 -0400

    cleaned up code

--- Scratch/week2/scratch_1.py
+++ Scratch/week2/scratch_1.py
@@ -1,18 +1,24 @@
-scores = [72,88,91,100,81]
+#example code using Map and Filter
 
-def lettergrade(n):
+scores = [72, 88, 91, 100, 81]
+
+
+def letter_grade(n):
     return 'A' if n >= 90 else 'B' if n >= 80 else 'C' if n >= 70 else 'D' if n >= 60 else 'F'
 
-grades = map(lettergrade, scores)
+
+grades = map(letter_grade, scores)
+
 
 def passed(n):
-    return True if n in ["A","B","C"] else False
+    return True if n in ["A", "B", "C"] else False
 
-passgrades =  filter(passed,grades)
+
+passing_grades = filter(passed, grades)
 
 for grade in grades:
-    print (grade)
+    print(grade)
 
-for pg in passgrades:
+for pg in passing_grades:
     print ("passed:"+pg)
 
