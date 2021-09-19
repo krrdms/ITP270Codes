b5a7e3351181e0fbda61be91bef7fff72c58024e
JKA <keirre.adams@gmail.com>
2021-09-19 07:42:45 -0400

    Example password cracker for Linux using crypt

---
+++ DictionaryPasswordCracker Example/UnixCryptCracker.py
@@ -0,0 +1,28 @@
+import crypt
+
+
+def testPass(cryptPass):
+    salt = cryptPass[0:2]
+    dictfile = open('dictionary.txt')
+    for word in dictfile.readlines():
+        word = word.strip('\n')
+        cryptWord = crypt.crypt(word, salt)
+        if (cryptWord == cryptPass):
+            print("[+] Found Password:", word)
+            return
+    print("[-] Password Not Found")
+    return
+
+
+def main():
+    passFile = open('passwords.txt')
+    for line in passFile.readlines():
+        if ":" in line:
+            user = line.split(":")[0]
+            cryptPass = line.split(":")[1].strip(' ')
+            print("[*] Cracking Password for: ", user)
+            testPass(cryptPass)
+
+
+if __name__ == "__main__":
+    main()
