432250ca5856ac45bd288db70a61a346de712e6d
JKA <keirre.adams@gmail.com>
2021-09-19 07:49:26 -0400

    Cleaned up code

--- Scratch/week3/25Itertools.py
+++ Scratch/week3/25Itertools.py
@@ -2,11 +2,25 @@ import itertools
 import string
 
 password = "g3T!"
+max_password_length = 8
 
-for guess in itertools.product(string.digits+string.ascii_lowercase+string.ascii_uppercase+"!@#$%^&*+"+"",
-                                   repeat=4):
-    guess = ''.join(guess)
-    print(guess)
-    if guess == password:
-        print(guess)
-        break
+def crackPass(max_password_length):
+    # global password
+    chars = 0
+    while chars < max_password_length:
+        chars += 1
+        for guess in itertools.product(
+                string.digits + string.ascii_lowercase + string.ascii_uppercase + "!@#$%^&*+" + "",
+                repeat=chars):
+            guess = ''.join(guess)
+            print("[-]Try:", guess)
+            if guess == password:
+                print("[*]Hit:", guess)
+                return
+
+
+crackPass(max_password_length)
+
+
+if __name__ == "__main__":
+    crackPass(max_password_length)
